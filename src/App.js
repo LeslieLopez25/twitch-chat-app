e68cca4e96f365f9d78fb41df2436b27641df3fb
Leslie Lopez <leslie2515lopez@gmail.com>
2022-08-24 22:15:00 -0500

    Setting up the app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,42 @@
-function App() {
-  return <div className="App"></div>;
-}
+import { StreamChat } from "stream-chat";
+import {
+  Chat,
+  Channel,
+  ChannelHeader,
+  ChannelList,
+  MessageList,
+  MessageInput,
+  Thread,
+  Window,
+} from "stream-chat-react";
+
+const client = new StreamChat("yf7e3c6uy7m5b");
+
+client.connectUser(
+  {
+    id: "dave-matthews",
+    name: "Dave Matthews",
+  },
+  eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiZGF2ZS1tYXR0aGV3cyJ9
+    .dc7 - QjQfOffOCGCRQDEP2rClSlnni7LGzoiLOxyVDyY
+);
+
+const channel = client.channel("gaming", {
+  name: "Gaming Demo",
+  members: ["dave-matthews", "trey-anastasio"],
+});
+
+const App = () => (
+  <Chat client={client}>
+    <Channel channel={channel}>
+      <Window>
+        <ChannelHeader />
+        <MessageList />
+        <MessageInput />
+      </Window>
+      <Thread />
+    </Channel>
+  </Chat>
+);
 
 export default App;
