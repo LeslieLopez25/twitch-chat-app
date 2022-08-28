ff12bb516e878ae761b5b9f59a52fce4e743cb9b
Leslie Lopez <leslie2515lopez@gmail.com>
2022-08-27 23:14:28 -0500

    Building the chat UI

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useEffect, useState } from "react";
 import { StreamChat } from "stream-chat";
 import {
   Chat,
@@ -9,34 +10,57 @@ import {
   Thread,
   Window,
 } from "stream-chat-react";
+import "@stream-io/stream-chat-css/dist/css/index.css";
 
-const client = new StreamChat("yf7e3c6uy7m5b");
+const filters = { type: "messaging" };
+const options = { state: true, presence: true, limit: 10 };
+const sort = { last_message_at: -1 };
 
-client.connectUser(
-  {
-    id: "dave-matthews",
-    name: "Dave Matthews",
-  },
-  eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiZGF2ZS1tYXR0aGV3cyJ9
-    .dc7 - QjQfOffOCGCRQDEP2rClSlnni7LGzoiLOxyVDyY
-);
+const client = StreamChat.getInstance("f7e3c6uy7m5b");
 
-const channel = client.channel("gaming", {
-  name: "Gaming Demo",
-  members: ["dave-matthews", "trey-anastasio"],
-});
+const App = () => {
+  const [clientReady, setClientReady] = useState(false);
+  const [channel, setChannel] = useState(null);
 
-const App = () => (
-  <Chat client={client}>
-    <Channel channel={channel}>
-      <Window>
-        <ChannelHeader />
-        <MessageList />
-        <MessageInput />
-      </Window>
-      <Thread />
-    </Channel>
-  </Chat>
-);
+  useEffect(() => {
+    const setupClient = async () => {
+      try {
+        await client.connectUser(
+          {
+            id: "dave-matthews",
+            name: "Dave Matthews",
+          },
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiZGF2ZS1tYXR0aGV3cyJ9.dc7-QjQfOffOCGCRQDEP2rClSlnni7LGzoiLOxyVDyY"
+        );
+        const channel = await client.channel("gaming", "gaming-demo", {
+          name: "Gaming Demo",
+        });
+        setChannel(channel);
+
+        setClientReady(true);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    setupClient();
+  }, []);
+
+  if (!clientReady) return null;
+
+  return (
+    <Chat client={client} darkMode={true}>
+      <ChannelList filters={filters} sort={sort} options={options} />
+      <Channel channel={channel}>
+        <Window>
+          <ChannelHeader />
+          <MessageList />
+          <MessageInput />
+        </Window>
+        <Thread />
+      </Channel>
+    </Chat>
+  );
+};
 
 export default App;
